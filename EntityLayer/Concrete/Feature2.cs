be3197c0db5ea7cbba8d4bc909c85a547e0704dc
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-12-02 19:39:39 +0300

    ders sonu 7: Conext Sınıfı

--- EntityLayer/Concrete/Feature2.cs
+++ EntityLayer/Concrete/Feature2.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class Featuer2
+    public class Feature2
     {
+        [Key]
         public int Feature2ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
