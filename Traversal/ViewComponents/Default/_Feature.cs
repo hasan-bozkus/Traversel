ad0c5d0b765e5d9af2bac50805e7298a7a914f2f
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-12-03 22:19:00 +0300

    ders sonu 17: Feature Statistics

---
+++ Traversal/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,17 @@
+﻿using BusinnesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EFFeatureDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
