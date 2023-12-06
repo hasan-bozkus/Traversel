0c9f32e255f26549eadf8460b1689ca9223fd622
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-12-06 16:18:04 +0300

    ders sonu 20: Tur Rotaları

---
+++ Traversal/Controllers/DestinationController.cs
@@ -0,0 +1,17 @@
+﻿using BusinnesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
