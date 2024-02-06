d11fc5172be4fe9620d1dea543498323b8d9f958
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-02-06 23:09:02 +0300

    ders sonu 28: Login ve Register Sayfaları

---
+++ Traversal/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Traversal.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> SignUp()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
