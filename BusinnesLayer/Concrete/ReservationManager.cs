ad3a325ae65cd61c5802015a579d0b42c5cff8c5
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-17 21:42:59 +0300

    Ders sonu 38: Rezervasyon Sayfası

---
+++ BusinnesLayer/Concrete/ReservationManager.cs
@@ -0,0 +1,46 @@
+using BusinnesLayer.Abtstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnesLayer.Concrete
+{
+	public class ReservationManager : IReservationService
+	{
+		IReservationDal _ReservationDal;
+
+		public ReservationManager(IReservationDal reservationDal)
+		{
+			_ReservationDal = reservationDal;
+		}
+
+		public void TAdd(Reservetion t)
+		{
+			_ReservationDal.Insert(t);
+		}
+
+		public void TDelete(Reservetion t)
+		{
+			_ReservationDal.Delete(t);
+		}
+
+		public Reservetion TGetByID(int id)
+		{
+			return  _ReservationDal.GetByID(id);
+		}
+
+		public List<Reservetion> TGetList()
+		{
+			return _ReservationDal.GetList();
+		}
+
+		public void TUpdate(Reservetion t)
+		{
+			_ReservationDal.Update(t);
+		}
+	}
+}
--- Traversal/Areas/Member/Controllers/ReservationController.cs
+++ Traversal/Areas/Member/Controllers/ReservationController.cs
@@ -1,11 +1,20 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinnesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Traversal.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDal());
+
+        ReservationManager reservationManager = new ReservationManager(new EFReservationDal());
+
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -19,13 +28,22 @@ namespace Traversal.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.values = values;
             return View();
         }
 
         [HttpPost]
         public IActionResult NewReservation(Reservetion p)
         {
-            return View();
+            p.AppUserID = 3;
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
