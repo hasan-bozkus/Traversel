a557e22dfb0e167181e8e3fe4309e8947be9e116
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-21 20:42:51 +0300

    Ders sonu 44:  Dashobardda Verilerin Listelenmesi

---
+++ Traversal/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using EntityLayer.Concrete;
+using System.Threading.Tasks;
+
+namespace Traversal.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.userFullName = values.Name + " " + values.Surname;
+            ViewBag.userPhoneNumber = values.PhoneNumber;
+            ViewBag.userEmail = values.Email;
+            return View();
+        }
+    }
+}
