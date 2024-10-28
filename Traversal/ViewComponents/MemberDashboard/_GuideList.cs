58755a8b98029f66264c4c1b549bdb5330e08ab2
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-28 19:28:45 +0300

    Ders sonu 45: Tur Rehberleri Listesi Ders sonu 46: Platform Ayarları View Component Ders sonu 47: Admin Areas

---
+++ Traversal/ViewComponents/MemberDashboard/_GuideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinnesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.MemberDashboard
+{
+    public class _GuideList : ViewComponent
+    {
+        GuideManager guideManager = new GuideManager(new EFGuideDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = guideManager.TGetList(); 
+            return View(values);
+        }
+    }
+}
