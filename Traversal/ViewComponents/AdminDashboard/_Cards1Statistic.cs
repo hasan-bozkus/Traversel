30c8f11132eee73adc2a1b393a485eec7672b135
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-11-03 19:10:27 +0300

    ders sonu 51: Admin Layout İkons, der sonu 52: Admin Dashboard 1

---
+++ Traversal/ViewComponents/AdminDashboard/_Cards1Statistic.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Traversal.ViewComponents.AdminDashboard
+{
+    public class _Cards1Statistic : ViewComponent
+    {
+        Context context = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.tursayisi = context.Destinations.Count();
+            ViewBag.misafirsayisi = context.Users.Count();
+            return View();
+        }
+    }
+}
