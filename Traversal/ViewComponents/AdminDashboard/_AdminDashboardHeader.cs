936c2f6882c26486fa5a859fb665c60ca10ce60e
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-11-04 20:44:57 +0300

    ders sonu  53: admin dashboard 2

---
+++ Traversal/ViewComponents/AdminDashboard/_AdminDashboardHeader.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Traversal.ViewComponents.AdminDashboard
+{
+    public class _AdminDashboardHeader : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Traversal/ViewComponents/AdminDashboard/_AdminGuideList.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Traversal.ViewComponents.AdminDashboard
+{
+    public class _AdminGuideList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
