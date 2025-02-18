c7fb2268f11e5b544cccd1fbda5b0628381b96da
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-02-18 21:47:03 +0300

    Ders sonu 88: SignalR ile Grafikte Anlık Veri Görüntüleme Ders sonu 89: Rol Listesi ve Rol Ekleme İşlemleri Ders sonu 90:  Rol Silme Rol Düzenleme ve Area Route Ayarları Ders sonu 91: Kullanıcıları Rolleri ile Listeleme

---
+++ Traversal/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,109 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Traversal.Areas.Admin.Models;
+
+namespace Traversal.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            AppRole role = new AppRole()
+            {
+                Name = createRoleViewModel.RoleName
+            };
+
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(createRoleViewModel);
+        }
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var role = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == id);
+            await _roleManager.DeleteAsync(role);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRole(int id)
+        {
+            var role = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == id);
+            UpdateRoleViewModel updateRoleViewModel = new UpdateRoleViewModel()
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+            return View(updateRoleViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel updateRoleViewModel)
+        {
+            var value = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == updateRoleViewModel.Id);
+
+            value.Name = updateRoleViewModel.RoleName;
+            await _roleManager.UpdateAsync(value);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var values = await _userManager.Users.ToListAsync();
+            return View(values);
+        }
+
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == id);
+            var roles = await _roleManager.Roles.ToListAsync();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel roleAssignViewModel = new RoleAssignViewModel();
+                roleAssignViewModel.RoleId = item.Id;
+                roleAssignViewModel.RoleName = item.Name;
+                roleAssignViewModel.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(roleAssignViewModel);
+            }
+            return View(roleAssignViewModels);
+        }
+    }
+}
